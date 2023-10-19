ecf74c45b9883ca0090d9f6bb26d6cbeac46620d
David <davidpchi@gmail.com>
2023-10-18 20:59:06 -0700

    Update the server to support updating favoriteCommander

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
-
 const express = require("express");
 const members = require("./members");
 const axios = require("axios");
 const mongoose = require("mongoose");
 
 const app = express();
+
+// MAKE SURE ALL REQUESTS HAVE CONTENT-TYPE: APPLICATION/JSON
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
+// connect to the db
 mongoose.connect(
     process.env.MONGODB_URI, 
     {
@@ -18,71 +20,72 @@ mongoose.connect(
     }
 );
 
-const studentSchema = new mongoose.Schema({
-    roll_no: {
+// initialize our db models
+const profileSchema = new mongoose.Schema({
+    _id: Number,
+    userId: {
         type: Number,
         required: true
     },
-    name: String,
-    year: Number,
-    subjects: [String]
+    favoriteCommander: String,
 });
 
-const Student = mongoose.model('Student', studentSchema);
+const Profile = mongoose.model('Profile', profileSchema);
 
-app.get("/", async (request, response) => {
-    const students = await Student.find({});
-    response.status(200).send(students);
+// handlers
 
-    // response.status(200).json({members: members});
+// returns all profiles 
+app.get("/profiles", async (request, response) => {
+    const profiles = await Profile.find({});
+    response.status(200).send(profiles);
 })
 
-app.post("/", (request, response) => {
-    const newMember = {
-        id: request.body.id,
-        name: request.body.name,
-        age: request.body.age
-    };
+// returns a profile for a specific user
+app.get("/profiles/:userId", async (request, response) => {
+    const profiles = await Profile.find({userId: request.params.userId});
+    response.status(200).send(profiles);
+})
 
-    members.push(newMember);
-
-    // MONGO DB TEST CODE:
-    // const stud = new Student({
-    //     roll_no: 1001,
-    //     name: 'Madison Hyde',
-    //     year: 3,
-    //     subjects: ['DBMS', 'OS', 'Graph Theory', 'Internet Programming']
-    // });
-    // stud
-    //     .save()
-    //     .then(
-    //         () => console.log("One entry added"), 
-    //         (err) => console.log(err)
-    //     );
-
-    response.status(200).json({members: members});
-});
-
-app.post("/test", async (request, response) => {
+// creates or updates a new profile
+app.post("/profiles", async (request, response) => {
     const result = await axios.get("https://discord.com/api/users/@me", {
-        headers: { authorization: `Bearer ${request.body.accessToken}` }
+        headers: { authorization: `Bearer ${request.headers["access-token"]}` }
     })
     const discordProfile = result.data;
 
-    if (discordProfile.id !== request.body.id) {
-        response.status(403).json({message: "Request id does not match access token claims."})
+    if (discordProfile.id !== request.body.userId) {
+        response.status(403).json({message: "Request id does not match access token claims."});
+        return;
     }
-    else {
-        response.status(200).json(discordProfile);
-    }
+
+    const profile = new Profile({
+        userId: request.body.userId,
+        favoriteCommander: request.body.favoriteCommander,
+    });
+
+    const objId = Number(request.body.userId);
+    
+    Profile.findOneAndUpdate(
+        {_id: objId},
+        {
+            _id: objId,
+            favoriteCommander: request.body.favoriteCommander,
+            userId: request.body.userId,
+        },
+        {upsert: true, new: true, setDefaultsOnInsert: true}
+    ).then(
+        () => console.log("One entry added"),
+        (err) => console.log(err)
+    );
+    
+    response.status(200).json(profile);
 });
 
-
-app.delete("/:id", (request, response) => {
-    const filteredMembers = members.filter(
-        member => member.id !== parseInt(request.params.id)
-    );
-    response.status(200).json({members: filteredMembers });
-})
+// app.delete("/:userId", (request, response) => {
+//     const filteredMembers = members.filter(
+//         member => member.id !== parseInt(request.params.id)
+//     );
+//     response.status(200).json({members: filteredMembers });
+// })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
