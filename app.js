55c3c77da9899aa0b56f1391d943f82c0966f6b4
David <davidpchi@gmail.com>
2024-08-11 12:30:11 -0700

    More logs

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.get("/archidekt/profile/:archidektId", async (request, response) => {
         const results = archidektData !== undefined && archidektData.results !== undefined ? archidektData.results : [];
 
         if (results.length > 0) {
-            throw new Error("Results are missing.");
+            throw new Error(`Results are missing.${request.params.archidektId}.${archidektData}.`);
         }
 
         const firstResult = results[0];
